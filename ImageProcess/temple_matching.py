f7660fbcd6734776fbedce9d9a7299c79c64cf93
Xuyining <oxuyining@gmail.com>
2018-03-20 16:28:01 +0800

    new class TempleMatch

---
+++ ImageProcess/temple_matching.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+
+class TempleMatching(object):
+    def __init__(self):
+        pass
+
+    def normal_match(self):
+        pass
+
+    def sift_match(self):
+        pass
