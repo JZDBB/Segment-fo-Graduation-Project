686026400d18092d5d59e411d302131308deba70
Xuyining <oxuyining@gmail.com>
2018-03-21 17:05:17 +0800

    flow chart in main function

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-from ImageProcess import temple_match
+from ImageProcess import temple_match, data, nms
+
+def main():
+    # read the template picture
+
+    # read the picture which need segment
+
+    # judge its rotate and scale
+
+    # ajust its size to a Regulated template
+
+    # use the normal template match to recognise its frame   \
+    #                                                         >  this need further consider
+    # use the SIFT template match to recognise its frame     |
+
+    # use its frame and picture to segment
+
+    # save the segment picture
+
+
+    pass
+
+
+if __name__ == '__main__':
+    main()
