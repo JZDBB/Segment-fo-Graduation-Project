b225260e1038a78cf845852e7e38ef415d1aab15
Xuyining <oxuyining@gmail.com>
2018-03-22 10:10:08 +0800

    ajust test ok

--- main.py
+++ main.py
@@ -12,25 +12,27 @@ class SegMain(object):
         template = self.data.read_images(template_path, None)
         # read the picture which need segment
         segdatas = self.data.read_images(segdata_path, None)
+        # judge its rotate and scale (未完成 思考中)
+
         for segdata in segdatas:
-            cv2.imshow('', segdata)
+            # ajust its size to a Regulated template
+            ratio0 = 1.0
+            if segdata.shape[1] < 2500:
+                ratio0 = segdata.shape[1] / 2500.0
+                img = cv2.resize(segdata, (2500, int(2500 * segdata.shape[0] / segdata.shape[1])))
+
+            img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            cv2.imshow('', img_gray)
             cv2.waitKey(0)
-            # plt.imshow(segdata, cmap='gray')
-            # plt.show()
-        print("over")
-        # judge its rotate and scale
 
-        # ajust its size to a Regulated template
-        # for segdata in segdatas:
+            # use the normal template match to recognise its frame   \
+            #                                                         >  this need further consider
+            # use the SIFT template match to recognise its frame     |
 
 
-        # use the normal template match to recognise its frame   \
-        #                                                         >  this need further consider
-        # use the SIFT template match to recognise its frame     |
+            # use its frame and picture to segment
 
-        # use its frame and picture to segment
-
-        # save the segment picture
+            # save the segment picture
 
 
     pass
